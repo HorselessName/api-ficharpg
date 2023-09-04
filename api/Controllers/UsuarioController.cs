3905fc08aceea085ed3ea62115b7e4c35067c070
Guts <Guts@PR02-302-12.CSED.LAB>
2023-09-04 19:33:42 -0300

    Criei a Model vazia e a Controller.

---
+++ api/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/usuario")]
+public class UsuarioController : ControllerBase
+{
+    private static List<Usuario> usuarios = new List<Usuario>();
+
+    //GET: api/produto/listar
+    [HttpGet]
+    [Route("listar")]
+    public IActionResult Listar() =>
+        usuarios.Count == 0 ? NotFound() : Ok(usuarios);
+
+    //GET: api/produto/buscar/{bolacha}
+    [HttpGet]
+    [Route("buscar/{nome}")]
+    public IActionResult Buscar([FromRoute] string nome)
+    {
+        foreach (Usuario usuarioCadastrado in usuarios)
+        {
+            if (usuarioCadastrado.Nome == nome)
+            {
+                return Ok(usuarioCadastrado);
+            }
+        }
+        return NotFound();
+    }
+
+    //POST: api/usuario/cadastrar
+    [HttpPost]
+    [Route("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Usuario usuario)
+    {
+        usuarios.Add(usuario);
+        return Created("", usuario);
+    }
+
+}
---
+++ api/Models/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Models
+{
+    public class Usuario
+    {
+        // Forma abreviada: Cria atributo privado, e seta os getters/setters.
+        public string? Nome { get; set; }
+    }
+}
