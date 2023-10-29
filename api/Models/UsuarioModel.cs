45dad967e82162038b6b0485161fee88fa8e33b4
Raul Chiarella <RaulPotter11@Gmail.com>
2023-10-29 02:28:21 -0300

    Implementacoes: Relacionamento Ficha de RPG e Usuario; Data Annotations do EF; Tratativa de Erros Globais e das Controllers; Reorganização de Algumas Lógicas para Facilitar a Geração de Diagramas com o Code Mapper;

---
+++ api/Models/UsuarioModel.cs
@@ -0,0 +1,35 @@
+﻿#nullable disable
+
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    /// <summary>
+    /// Model Class <c>Usuario</c> é como o objeto é representado e cadastrado no banco de dados.
+    /// </summary>
+    public class UsuarioModel
+    {
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAtualizado { get; set; }
+        public bool? Deletado { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long IdUsuario { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public UsuarioModel()
+        {
+            DataCriacao = DateTime.Now;
+            Deletado = false;
+        }
+    }
+}
---
+++ api/ViewModels/FichaRpgViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace api.ViewModels
+{
+    public class FichaRpgViewModel
+    {
+        [SwaggerSchema(ReadOnly = true)]
+        public int IdFichaRpg { get; set; }
+        public int Nível { get; set; }
+        public string Antecedente { get; set; }
+        public string NomeDoJogador { get; set; }
+        public string Raça { get; set; }
+        public string Alinhamento { get; set; }
+        public int PontosDeExperiência { get; set; }
+        public long IdUsuario { get; set; }
+    }
+}
