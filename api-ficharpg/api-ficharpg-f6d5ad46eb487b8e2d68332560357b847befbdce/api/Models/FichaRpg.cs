698a670c3ddcab0b76ca9efb4c004b17edf222d8
Matheus.G <matheus1912.bat@gmail.com>
2023-11-19 17:36:49 -0300

    BackEnd

---
+++ api-ficharpg/api-ficharpg-f6d5ad46eb487b8e2d68332560357b847befbdce/api/Models/FichaRpg.cs
@@ -0,0 +1,22 @@
+﻿namespace api.Models
+{
+    public class FichaRpg
+    {
+        public int IdFichaRpg { get; set; }
+        public int Nivel { get; set; }
+        public string Antecedencia { get; set; }
+        public string NomeDoJogador { get; set; }
+        public string Raca { get; set; }
+        public string Alinhamento { get; set; }
+        public int PontosDeExperiencia { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAtualizado { get; set; } // Alterado para DateTime?
+        public bool? Deletado { get; set; }
+
+        // Referenciar um ID para o ModelBuilder - Se relacionar com o Usuario é obrigatório
+        public long IdUsuario { get; set; }
+
+        
+    }
+}
